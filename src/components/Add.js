cd90d20ec6329204bb0a1de06d327a9ee15c4cef
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-02-15 10:44:27 -0500

    Adding files and styling

---
+++ src/components/Add.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Add() {
+  return (
+    <div>
+      {/* <!-- Go Back to Home Page  --> */}
+      <a
+        class="btn-floating btn-large waves-effect waves-light blue back-button"
+        href="./index.html"
+      >
+        <i class="material-icons right">arrow_forward</i>
+      </a>
+
+      {/* <!-- Fighter Form Creation --> */}
+    </div>
+  );
+}
+
+export default Add;
---
+++ src/components/Battle.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Battle() {
+  return (
+    <div>
+      {/* Go Back to Home Page */}
+      <a
+        class="btn-floating btn-large waves-effect waves-light red back-button"
+        href="./index.html"
+      >
+        <i class="material-icons right">arrow_back</i>
+      </a>
+      {/* Charecter Selection */}
+    </div>
+  );
+}
+
+export default Battle;
